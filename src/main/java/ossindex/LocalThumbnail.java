eff3a8c42092c014924e2393930ef15105bb86ca
stockcode <stockcode@sina.cn>
2014-02-10 16:53:54 +0800

    完善了本地缩略图的功能

--- src/main/java/ossindex/LocalThumbnail.java
+++ src/main/java/ossindex/LocalThumbnail.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.*;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -22,6 +23,7 @@ public class LocalThumbnail {
     static long K1 = 1024;
     static double K100 = K1 * 100;
     static double K5 = K1 * 10;
+    static DecimalFormat df = new DecimalFormat("#0.00");
 
     public static void main(String[] args) throws IOException {
         String path = args[0];
@@ -70,7 +72,7 @@ public class LocalThumbnail {
 
     private static void thumbBig(String path) throws IOException {
         System.err.println("start bigthumb:" + path);
-        double scale = 0.5f;
+
 
         byte[] bytes;
 
@@ -78,7 +80,7 @@ public class LocalThumbnail {
         Iterator<File> iter = FileUtils.iterateFiles(new File(path), new String[]{"jpg", "JPG"}, true);
         File file;
         while(iter.hasNext()) {
-
+            double scale = 0.5f;
             file = iter.next();
             if (!file.getPath().contains("original")) continue;
 
@@ -95,12 +97,12 @@ public class LocalThumbnail {
 
             } while (Math.abs(bytes.length - K100) > K1 * 10  && i < 10);
 
-            System.err.println(file.getPath() + ":" + scale + "");
+            System.err.println("bigthumb:" + file.getPath() + ":" + df.format(scale) + "");
 
             if (file.getPath().contains("bigthumb"))
                 FileUtils.writeByteArrayToFile(file, bytes);
             else {
-                String filepath = file.getParent().replace("original", "bigthumb\\");
+                String filepath = file.getParent().replace("original", "bigthumb" + File.separator);
                 FileUtils.forceMkdir(new File(filepath));
                 FileUtils.writeByteArrayToFile(new File(filepath + file.getName()), bytes);
             }
@@ -135,12 +137,12 @@ public class LocalThumbnail {
 
             } while (Math.abs(bytes.length - K5) > K1 * 2  && i < 10);
 
-            System.err.println(file.getPath() + ":" + scale + "");
+            System.err.println("smallthumb" + file.getPath() + ":" + df.format(scale) + "");
 
             if (file.getPath().contains("smallthumb"))
                 FileUtils.writeByteArrayToFile(file, bytes);
             else {
-                String filepath = file.getParent().replace("original", "smallthumb\\");
+                String filepath = file.getParent().replace("original", "smallthumb" + File.separator);
                 FileUtils.forceMkdir(new File(filepath));
                 FileUtils.writeByteArrayToFile(new File(filepath + file.getName()), bytes);
             }
@@ -168,7 +170,7 @@ public class LocalThumbnail {
 
                 if (!file.getPath().contains("original")) {
 
-                    String filepath = file.getParent() + "\\original\\";
+                    String filepath = file.getParent() + File.separator + "original" + File.separator;
                     FileUtils.forceMkdir(new File(filepath));
                     filepath += file.getName().toLowerCase();
                     file.renameTo(new File(filepath));
@@ -190,7 +192,7 @@ public class LocalThumbnail {
 
                 int len = bytes.length;
 
-                System.err.println(file.getPath() + ":" + scale + ":" + len);
+                System.err.println(file.getPath() + ":" + df.format(scale) + ":" + len);
 
                 if (len - MB1 > 0) scale -= 0.09;
                 else scale += 0.09;
@@ -200,7 +202,7 @@ public class LocalThumbnail {
             if (file.getPath().contains("original"))
                 FileUtils.writeByteArrayToFile(file, bytes);
             else {
-                String filepath = file.getParent() + "\\original\\";
+                String filepath = file.getParent() + File.separator + "original" + File.separator;
                 FileUtils.forceMkdir(new File(filepath));
                 FileUtils.writeByteArrayToFile(new File(filepath + file.getName()), bytes);
                 FileUtils.forceDelete(file);
