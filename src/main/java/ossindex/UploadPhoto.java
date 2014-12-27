453f9b3529799c103d2d246c99a2458566b74066
耿科 <geng_ke@sina.com>
2014-12-27 20:04:15 +0800

    完善上传文件名

--- src/main/java/ossindex/UploadPhoto.java
+++ src/main/java/ossindex/UploadPhoto.java
@@ -16,6 +16,7 @@ import org.json.JSONException;
 
 import java.io.*;
 import java.util.Iterator;
+import java.util.UUID;
 
 /**
  * Created by Administrator on 13-7-11.
@@ -86,7 +87,7 @@ public class UploadPhoto {
                 byte[] bytes = FileUtils.readFileToByteArray(f);
                 InputStream is = new ByteArrayInputStream(bytes);
 
-                String key = StringUtils.replace(f.getAbsolutePath(), path, "").toLowerCase();
+                String key = StringUtils.replace(f.getParent(), path, "").toLowerCase() + "/" + generateShortUuid() + ".jpg";
 
                 key = StringUtils.replace(key, "\\", "/");
 
@@ -110,5 +111,25 @@ public class UploadPhoto {
 
         // 打印ETag
         logger.info(key + ":" + result.getETag());
+    }
+
+    public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z" };
+
+
+    public static String generateShortUuid() {
+        StringBuffer shortBuffer = new StringBuffer();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            shortBuffer.append(chars[x % 0x3E]);
+        }
+        return shortBuffer.toString();
+
     }
 }
